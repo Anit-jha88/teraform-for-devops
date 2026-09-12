287b8e6fbfa93e61c6e371a3b08623a9e3fa4314
Anit kumar jha <jhaanit.php@gmail.com>
2026-09-12 16:35:14 +0530

    modyfycode

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,11 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "clouddevops-bucket-s3"
+    key = "terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "clouddevops-table"
+  }
 }
