5ed3cae09c756f52fc5b990d644f2eac031b98fb
Anit kumar jha <73160412+Anit-jha88@users.noreply.github.com>
2026-09-19 10:47:16 +0530

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install nginx -y
 
-sudo systemctl start nginx
+sudo systemctl start nginx 
 sudo systemctl enable nginx
 
 echo "<h1>Nginx installation completed and service started.</h1>" | sudo tee /var/www/html/index.html
